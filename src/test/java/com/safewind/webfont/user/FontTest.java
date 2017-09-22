1933f8ba0db0341669aef16bd2c8a700a9a10474
sean <2775786355@qq.com>
2017-09-22 17:20:28 +0800

    练习使用了一下junit

--- src/test/java/com/safewind/webfont/user/FontTest.java
+++ src/test/java/com/safewind/webfont/user/FontTest.java
@@ -2,6 +2,7 @@ package com.safewind.webfont.user;
 
 import com.safewind.webfont.dao.CollectionDao;
 import com.safewind.webfont.dao.CommentDao;
+import com.safewind.webfont.dao.FontDao;
 import com.safewind.webfont.service.FontService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,13 +12,18 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @ContextConfiguration("classpath:spring-mybatis.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
-public class UtilTest {
+public class FontTest {
 
 
+    @Autowired
+    private FontService fontService;
+    @Autowired
+    private CollectionDao collectionDao;
+    @Autowired
+    private CommentDao commentDao;
+    @Autowired
+    private FontDao fontDao;
 //    测试分页所要计算的数据
-//@Autowired
-//private FontService fontService;
-//
 //    @Test
 //    public void testPagingCalculate()
 //    {
@@ -27,12 +33,6 @@ public class UtilTest {
 
 //  测试用户收藏该字体
 
-    @Autowired
-    private FontService fontService;
-    @Autowired
-    private CollectionDao collectionDao;
-    @Autowired
-    private CommentDao commentDao;
     @Test
     public void test()
     {
@@ -49,9 +49,16 @@ public class UtilTest {
 //        System.out.println("==============字体列表=============="+fontService.getFontList());
 
 //        System.out.println("++++++++++引用他人的评论（通过评论的编号）+++++++++++++++++++回复的用户名"+fontService.replyOthersComment(1)+"用户的评论"+fontService.quoteOthersComment(1));
+//          测试给用户添加该字体收藏
+//            fontService.addCollectionFont("ahui",7);
 
 
-
+//        System.out.println("============测试用户是否收藏该字体================"+fontService.isCollectionFont(1,"ahui"));
+//          执行用户删除自己的评论(注意是假删除)
+//        fontService.deleteComment(3,"ahui");
+//        测试添加用户推荐该字体到数据库
+//        fontService.addRecommendationFont("ahui",5);
+//        System.out.println("+++++++++++++++测试用户是否推荐该字体+++++++++++++++++++++"+fontService.isRecommendationFont(5,"ahui"));
     }
 
 
