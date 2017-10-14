ec010c4fd2ed98cec9e019a3b07647fad591ae35
sean <2775786355@qq.com>
2017-10-14 23:56:22 +0800

    添加了一些提示信息的常值

--- src/main/java/com/safewind/webfont/constant/DBMsgConstant.java
+++ src/main/java/com/safewind/webfont/constant/DBMsgConstant.java
@@ -1,6 +1,5 @@
 package com.safewind.webfont.constant;
 
 public class DBMsgConstant {
-
-	public static final String HH_ADMIN_USER_CONTENT = "这是一个用户";
+	public static final String HH_ADMIN_USER_CONTENT = "这是一个字体用户";
 }
---
+++ src/main/java/com/safewind/webfont/constant/MsgAlertConstant.java
@@ -0,0 +1,10 @@
+package com.safewind.webfont.constant;
+
+/**
+ * 用于返回前台具体的提示信息
+ */
+public class MsgAlertConstant {
+    public static final String USER_HASED_COLLECTION_FONT="已经收藏该字体";
+    public static final String USER_COLLECTION_FONT_SUCCESS="收藏该字体成功";
+    public static final String USER_COLLECTION_FONT_FAIL ="收藏该字体失败，请稍后重试";
+}
