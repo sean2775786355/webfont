9d922d2d1f4fd56acd2a059756fdda972b1e70fc
sean <2775786355@qq.com>
2017-10-16 00:04:42 +0800

    添加了一些修改顾客信息的提示语

--- src/main/java/com/safewind/webfont/constant/MsgAlertConstant.java
+++ src/main/java/com/safewind/webfont/constant/MsgAlertConstant.java
@@ -7,4 +7,9 @@ public class MsgAlertConstant {
     public static final String USER_HASED_COLLECTION_FONT="已经收藏该字体";
     public static final String USER_COLLECTION_FONT_SUCCESS="收藏该字体成功";
     public static final String USER_COLLECTION_FONT_FAIL ="收藏该字体失败，请稍后重试";
+    public static final String CONSUMER_CHANGE_PHONE_FAIL="修改手机号失败";
+    public static final String CONSUMER_CHANGE_PHONE_SUCCESS="修改手机号成功";
+    public static final String CONSUMER_CHANGE_REPOST_FAIL="修改个人信息失败";
+    public static final String CONSUMER_CHANGE_REPOST_SUCCESS="修改个人信息成功";
+
 }
